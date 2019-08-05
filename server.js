496406cca81f23c2c245ef49aaa04726838092e9
Sam Wang <sam.zz.wang@gmail.com>
2019-08-04 19:05:35 -0700

    Prep for deploy

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const rp = require('request-promise');
+const path = require('path');
 const data = require('./data');
 const config = require('./config');
+
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -34,5 +36,15 @@ app.post('/api/subscribe', async (req, res) => {
     res.status(500).send(e);
   }
 })
+
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+
+  // Handle React routing, return all requests to React app
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
