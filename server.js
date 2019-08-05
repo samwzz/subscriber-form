ba7b9296c496e100aa58c263cd944c25741124f9
Sam Wang <sam.zz.wang@gmail.com>
2019-08-04 19:41:54 -0700

    Fix root path

--- server.js
+++ server.js
@@ -11,8 +11,6 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
-app.get('/', (req, res) => res.send('Hello World!'))
-
 app.get('/api/teams', (req, res) => {
   res.send(data);
 })
